6bd35ef054fda96366d0275dbc2a3a89711a8434
Jose Fernandez <joseferper@gmail.com>
2009-05-16 17:16:14 +0200

    Added the create method

--- lib/zimbra/calendar_item.rb
+++ lib/zimbra/calendar_item.rb
@@ -16,13 +16,51 @@ module Zimbra
     def all_day?
       return @attributes[:all_day] == "1"
     end
-    
+ 
+    # Creates a new appointment
+    #
+    #  :attributes
+    #    :addresses          => An array with Address instances the appointment is meant to
+    #    :subject   
+    #    :calendar_component => CalendarComponet
+    #    :start_time         => A datetime object at with the appointment will start
+    #    :end_time           => A datetime object at with the appointment will end
+    #    :duration           => An instances of a Duration object 
+    #    :atendees           => An array of Atendee instances
+    #    :organizer          => An Organizer instance
+   
     def self.create(credentials,attrs = {})
-      result = {}
-      attrs.each{|key,value| result[("xmlattr_"+key.to_s).to_sym] = value }
-      result[:m] = {:content => "pepe" ,:comp => { :xmlattr_p =>"pp"} }
-            return driver.CreateAppointmentRequest(credentials,result.merge({:xmlattr_pepe => "all"}))
+      request = SOAP::SOAPElement.new("CreateAppointmentRequest")
+      mail = SOAP::SOAPElement.new("m")
+      mail.add(SOAP::SOAPElement.new("su",attrs[:subject]))
+      invitation = SOAP::SOAPElement.new("inv")
+      component = SOAP::SOAPElement.new("comp")
+#      component.extraattr["d"] = Time.parse(attrs[:start_time].str_date).to_i.to_s
+      attrs[:addresses].each do |address|
+        mail.add(address.to_soap)
+      end
+      component.add(attrs[:start_time].to_soap)
+      component.add(attrs[:organizer].to_soap)
+      component.extraattr["fba"] = "F"
+      attrs[:atendees].each do |atendee|
+        component.add(atendee.to_soap)
+      end
+      component.add(attrs[:duration].to_soap)
+      invitation.add(component)
+      mail.add(invitation)
+      request.add(mail)
+      request.extraattr["xmlns"] = "urn:zimbraMail"
+      result = driver.CreateAppointmentRequest(credentials,request)
+      return result.first if result && result.first
     end
+
+
+#    def self.create(credentials,attrs = {})
+#      result = {}
+#      attrs.each{|key,value| result[("xmlattr_"+key.to_s).to_sym] = value }
+#      result[:m] = {:e =>1 , :comp => { :xmlattr_p =>"pp"} }
+#      return driver.CreateAppointmentRequest(credentials,result.merge({:xmlattr_pepe => "all"}))
+#    end
     
   end
 end
