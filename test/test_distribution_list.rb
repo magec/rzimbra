266e5d24253d2130bf3ff6d552d67e9b987f9714
Jose Fernandez <joseferper@gmail.com>
2009-05-19 20:16:09 +0200

    added test for the distribution list

---
+++ test/test_distribution_list.rb
@@ -0,0 +1,26 @@
+$:.unshift(File.dirname(__FILE__) + '/../lib').unshift(File.dirname(__FILE__))
+
+require 'test/unit'
+require 'rzimbra'
+require 'config/load_config'
+
+class TestAccount < Test::Unit::TestCase
+  
+  TEST_PASSWORD = APP_CONFIG["test_password"]
+  TEST_NAME     = APP_CONFIG["test_login"]
+  DIST_NAME  = APP_CONFIG["distribution_list"]
+
+  def setup
+    Zimbra::Base.config(:endpoint_url => APP_CONFIG["end_point_url"])
+    @credentials = Zimbra::Base.get_credentials(APP_CONFIG["admin_login"],APP_CONFIG["admin_password"])
+  end
+
+  def test_find
+    assert_not_nil Zimbra::DistributionList.find_by_name(@credentials,DIST_NAME), "Error se esperaba una lista"
+  end
+
+  def test_find_with_members
+    assert_not_nil Zimbra::DistributionList.find_by_name(@credentials,DIST_NAME)[0].members, "Error se esperaba una lista"
+  end
+
+end
