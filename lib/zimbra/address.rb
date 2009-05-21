83aa6f65ffc1579a662ef3ad272fbc2b267d3dde
U-Luis\Administrador <Administrador@Luis.(none)>
2009-05-21 14:43:06 -0300

    The constructor was deleted cause now is inherited

--- lib/zimbra/address.rb
+++ lib/zimbra/address.rb
@@ -1,17 +1,15 @@
 module Zimbra
-  # An abstraction of an email address as used by zimbra   
+  # An abstraction of an email address as used by zimbra
   class Address < Base
     # (f)rom, (t)o, (c)c, (b)cc, (r)eply-to, (s)ender
     #attr_accessor :type
     # The commeny/name part of an address
-    #attr_accessor :personal_name 
+    #attr_accessor :personal_name
     # If we have personal, first word in "word1 word2" format, or last word in "word1, word2" format.
     # if no personal, take string before "@" in email-address.
     #attr_accessor :display_name
     # The actual address as given by the user, the one that is going to be used when sending
     #attr_accessor :value
-    def initialize(attrs = {})
-      @attributes = attrs
-    end
+
   end
 end
